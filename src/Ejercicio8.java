98bd4136aab50c06503d0ed4a7099de64955242f
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-04 23:25:22 +0100

    Ejercicio 8 del UD5.

---
+++ src/Ejercicio8.java
@@ -0,0 +1,11 @@
+
+public class Ejercicio8 {
+	
+	public static void main(String args[]) {
+		
+		//Definimos el bucle y lo incrementamos.
+		for (int num = 1; num <= 100; num++) {
+			System.out.println(num);
+		}
+	}
+}
